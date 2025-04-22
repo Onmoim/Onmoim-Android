83b5e9b61e253db0ccdbbd7c8ed7a74d947a794e
Hong Sunghwi <tnfy10@gmail.com>
2025-04-22 23:46:13 +0900

    디자인 시스템 구현

--- app/src/main/java/com/onmoim/core/ui/theme/Color.kt
+++ app/src/main/java/com/onmoim/core/ui/theme/Color.kt
@@ -2,10 +2,14 @@ package com.onmoim.core.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val Color565656 = Color(0xFF565656)
+val Color69999999 = Color(0xFF999999)
+val Color2781FC = Color(0xFF2781FC)
+val Color21C3AB = Color(0xFF21C3AB)
+val ColorEF61DF = Color(0xFFEF61DF)
+val ColorWhite = Color(0xFFFFFFFF)
+val ColorF8F8F8 = Color(0xFFF8F8F8)
+val ColorCDCDCD = Color(0xFFCDCDCD)
+val ColorBlack = Color(0xFF000000)
+val ColorF97D7D = Color(0xFFF97D7D)
+val ColorFF0033 = Color(0xFFFF0033)
--- app/src/main/java/com/onmoim/core/ui/theme/Theme.kt
+++ app/src/main/java/com/onmoim/core/ui/theme/Theme.kt
@@ -1,57 +1,119 @@
 package com.onmoim.core.ui.theme
 
-import android.os.Build
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
-import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.Stable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.staticCompositionLocalOf
+import androidx.compose.ui.graphics.Color
 
-private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+internal val ColorPalette = OnmoimColors(
+    backgroundColor = ColorWhite,
+    c565656 = Color565656,
+    c69999999 = Color69999999,
+    c2781FC = Color2781FC,
+    c21C3AB = Color21C3AB,
+    cEF61DF = ColorEF61DF,
+    cF8F8F8 = ColorF8F8F8,
+    cCDCDCD = ColorCDCDCD,
+    cF97D7D = ColorF97D7D,
+    cFF0033 = ColorFF0033,
 )
 
-private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
-)
+internal val LocalOnmoimColors = staticCompositionLocalOf<OnmoimColors> {
+    error("No OnmoimColorPalette provided")
+}
 
 @Composable
 fun OnmoimTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
-    val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
+    val colorPalette = remember { ColorPalette }
 
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
+    CompositionLocalProvider(
+        LocalOnmoimColors provides colorPalette,
+        LocalTypography provides Typography
+    ) {
+        MaterialTheme(
+            colorScheme = MaterialTheme.colorScheme.copy(
+                background = ColorPalette.backgroundColor,
+                surface = ColorPalette.backgroundColor
+            ),
+            content = content
+        )
+    }
+}
+
+object OnmoimTheme {
+    val colors: OnmoimColors
+        @Composable
+        get() = LocalOnmoimColors.current
+    val typography: OnmoimTypography
+        @Composable
+        get() = LocalTypography.current
+}
+
+@Stable
+class OnmoimColors(
+    backgroundColor: Color,
+    c565656: Color,
+    c69999999: Color,
+    c2781FC: Color,
+    c21C3AB: Color,
+    cEF61DF: Color,
+    cF8F8F8: Color,
+    cCDCDCD: Color,
+    cF97D7D: Color,
+    cFF0033: Color,
+) {
+    var backgroundColor by mutableStateOf(backgroundColor)
+        private set
+    var c565656 by mutableStateOf(c565656)
+        private set
+    var c69999999 by mutableStateOf(c69999999)
+        private set
+    var c2781FC by mutableStateOf(c2781FC)
+        private set
+    var c21C3AB by mutableStateOf(c21C3AB)
+        private set
+    var cEF61DF by mutableStateOf(cEF61DF)
+        private set
+    var cF8F8F8 by mutableStateOf(cF8F8F8)
+        private set
+    var cCDCDCD by mutableStateOf(cCDCDCD)
+        private set
+    var cF97D7D by mutableStateOf(cF97D7D)
+        private set
+    var cFF0033 by mutableStateOf(cFF0033)
+        private set
+
+    fun update(other: OnmoimColors) {
+        backgroundColor = other.backgroundColor
+        c565656 = other.c565656
+        c69999999 = other.c69999999
+        c2781FC = other.c2781FC
+        c21C3AB = other.c21C3AB
+        cEF61DF = other.cEF61DF
+        cF8F8F8 = other.cF8F8F8
+        cCDCDCD = other.cCDCDCD
+        cF97D7D = other.cF97D7D
+        cFF0033 = other.cFF0033
     }
 
-    MaterialTheme(
-        colorScheme = colorScheme,
-        typography = Typography,
-        content = content
+    fun copy(): OnmoimColors = OnmoimColors(
+        backgroundColor = backgroundColor,
+        c565656 = c565656,
+        c69999999 = c69999999,
+        c2781FC = c2781FC,
+        c21C3AB = c21C3AB,
+        cEF61DF = cEF61DF,
+        cF8F8F8 = cF8F8F8,
+        cCDCDCD = cCDCDCD,
+        cF97D7D = cF97D7D,
+        cFF0033 = cFF0033,
     )
 }
