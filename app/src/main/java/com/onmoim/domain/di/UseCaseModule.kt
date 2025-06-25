285115489b27f48673a0bad9805e1c7585fb4c23
Hong Sunghwi <tnfy10@gmail.com>
2025-06-25 22:40:53 +0900

    SignUpUseCase 구현

--- app/src/main/java/com/onmoim/domain/di/UseCaseModule.kt
+++ app/src/main/java/com/onmoim/domain/di/UseCaseModule.kt
@@ -2,7 +2,9 @@ package com.onmoim.domain.di
 
 import com.onmoim.core.data.repository.AuthRepository
 import com.onmoim.core.data.repository.TokenRepository
+import com.onmoim.core.data.repository.UserRepository
 import com.onmoim.domain.usecase.SignInUseCase
+import com.onmoim.domain.usecase.SignUpUseCase
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -16,4 +18,10 @@ object UseCaseModule {
         authRepository: AuthRepository,
         tokenRepository: TokenRepository
     ) = SignInUseCase(authRepository, tokenRepository)
+
+    @Provides
+    fun provideSignUpUseCase(
+        userRepository: UserRepository,
+        tokenRepository: TokenRepository
+    ) = SignUpUseCase(userRepository, tokenRepository)
 }
