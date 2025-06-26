15a0fe93a1c6e48666491444777d0a2d62a00bcb
Hong Sunghwi <tnfy10@gmail.com>
2025-06-26 20:35:46 +0900

    AuthEventBus 추가

---
+++ app/src/main/java/com/onmoim/core/event/AuthEventBus.kt
@@ -0,0 +1,33 @@
+package com.onmoim.core.event
+
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.receiveAsFlow
+import javax.inject.Inject
+
+class AuthEventBus @Inject constructor() {
+    private val _event = Channel<AuthEvent>(Channel.BUFFERED)
+    val event = _event.receiveAsFlow()
+
+    suspend fun notifyAuthExpired() {
+        _event.send(AuthEvent.AuthExpired)
+    }
+
+    suspend fun notifySignOut() {
+        _event.send(AuthEvent.SignOut)
+    }
+
+    suspend fun notifyNotAuthenticated() {
+        _event.send(AuthEvent.NotAuthenticated)
+    }
+
+    suspend fun notifyAuthenticated() {
+        _event.send(AuthEvent.Authenticated)
+    }
+}
+
+sealed class AuthEvent {
+    data object AuthExpired : AuthEvent()
+    data object SignOut : AuthEvent()
+    data object NotAuthenticated : AuthEvent()
+    data object Authenticated : AuthEvent()
+}
