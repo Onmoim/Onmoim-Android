e376abe42ed6a445c6b764ef48eb6ae3e8ef60cf
Hong Sunghwi <tnfy10@gmail.com>
2025-06-26 22:51:21 +0900

    인증 정보에 따른 화면 이동 핸들링 추가

--- app/src/main/java/com/onmoim/OnmoimAppState.kt
+++ app/src/main/java/com/onmoim/OnmoimAppState.kt
@@ -7,24 +7,51 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.navigation.NavDestination
 import androidx.navigation.NavHostController
-import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navOptions
+import com.onmoim.core.data.repository.TokenRepository
+import com.onmoim.core.data.repository.UserRepository
+import com.onmoim.core.event.AuthEvent
+import com.onmoim.core.event.AuthEventBus
+import com.onmoim.core.ui.navigation.navigateToHome
+import com.onmoim.feature.login.navigation.navigateToLogin
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 @Composable
 fun rememberOnmoimAppState(
-    navController: NavHostController = rememberNavController()
+    navController: NavHostController,
+    coroutineScope: CoroutineScope,
+    tokenRepository: TokenRepository,
+    userRepository: UserRepository,
+    authEventBus: AuthEventBus
 ): OnmoimAppState {
-    return remember(navController) {
+    return remember(
+        navController,
+        coroutineScope
+    ) {
         OnmoimAppState(
-            navController = navController
+            navController = navController,
+            coroutineScope = coroutineScope,
+            tokenRepository = tokenRepository,
+            userRepository = userRepository,
+            authEventBus = authEventBus
         )
     }
 }
 
 @Stable
-class OnmoimAppState (
-    internal val navController: NavHostController
+class OnmoimAppState(
+    internal val navController: NavHostController,
+    internal val coroutineScope: CoroutineScope,
+    internal val tokenRepository: TokenRepository,
+    internal val userRepository: UserRepository,
+    internal val authEventBus: AuthEventBus
 ) {
     private val previousDestination = mutableStateOf<NavDestination?>(null)
+
+    var shouldShowSplash = true
+        private set
 
     val currentDestination: NavDestination?
         @Composable get() {
@@ -37,4 +64,56 @@ class OnmoimAppState (
                 }
             } ?: previousDestination.value
         }
+
+    init {
+        handleAuthEvent()
+        checkAuthentication()
+    }
+
+    private fun handleAuthEvent() {
+        coroutineScope.launch {
+            authEventBus.event.collect { event ->
+                when (event) {
+                    AuthEvent.AuthExpired -> {
+                        navController.navigateToLogin()
+                    }
+
+                    AuthEvent.Authenticated -> {
+                        navController.navigateToHome(
+                            navOptions = navOptions {
+                                popUpTo(0) {
+                                    inclusive = true
+                                }
+                                launchSingleTop = true
+                            }
+                        )
+                        delay(30)
+                        shouldShowSplash = false
+                    }
+
+                    AuthEvent.NotAuthenticated -> {
+                        shouldShowSplash = false
+                    }
+
+                    AuthEvent.SignOut -> {
+                        navController.navigateToLogin()
+                    }
+                }
+            }
+        }
+    }
+
+    private fun checkAuthentication() {
+        coroutineScope.launch {
+            val accessToken = tokenRepository.getAccessToken()
+            val refreshToken = tokenRepository.getRefreshToken()
+            val hasNotInterest = userRepository.hasNotInterest()
+
+            if (accessToken == null || refreshToken == null || hasNotInterest) {
+                authEventBus.notifyNotAuthenticated()
+            } else {
+                authEventBus.notifyAuthenticated()
+            }
+        }
+    }
 }
