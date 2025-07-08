cf11242420ba3e8f55576f5c6f2d59d13bedaf54
Hong Sunghwi <tnfy10@gmail.com>
2025-07-08 16:01:29 +0900

    navigateToHome import 수정

--- app/src/main/java/com/onmoim/feature/login/navigation/LoginNavigation.kt
+++ app/src/main/java/com/onmoim/feature/login/navigation/LoginNavigation.kt
@@ -8,7 +8,7 @@ import androidx.navigation.NavOptions
 import androidx.navigation.compose.composable
 import androidx.navigation.navOptions
 import androidx.navigation.navigation
-import com.onmoim.navigation.navigateToHome
+import com.onmoim.feature.home.navigateToHome
 import com.onmoim.feature.login.view.InterestSelectRoute
 import com.onmoim.feature.login.view.LocationSettingRoute
 import com.onmoim.feature.login.view.LoginRoute
--- app/src/main/java/com/onmoim/ui/OnmoimAppState.kt
+++ app/src/main/java/com/onmoim/ui/OnmoimAppState.kt
@@ -12,8 +12,8 @@ import com.onmoim.core.data.repository.AppSettingRepository
 import com.onmoim.core.data.repository.TokenRepository
 import com.onmoim.core.event.AuthEvent
 import com.onmoim.core.event.AuthEventBus
+import com.onmoim.feature.home.navigateToHome
 import com.onmoim.feature.login.navigation.navigateToLogin
-import com.onmoim.navigation.navigateToHome
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
