cfc2b2c49fb120f9daf9fd9d08b8fd4a6056a3f6
Hong Sunghwi <tnfy10@gmail.com>
2025-08-02 19:25:15 +0900

    GroupChatSocket DI 모듈 추가, send destination 수정, 시스템 토픽 구독 추가, url 수정

--- core/network/src/main/java/com/onmoim/core/network/di/SocketModule.kt
+++ core/network/src/main/java/com/onmoim/core/network/di/SocketModule.kt
@@ -1,18 +1,28 @@
 package com.onmoim.core.network.di
 
+import com.onmoim.core.datastore.DataStorePreferences
+import com.onmoim.core.network.socket.GroupChatSocket
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import org.hildan.krossbow.stomp.StompClient
 import org.hildan.krossbow.websocket.okhttp.OkHttpWebSocketClient
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-object StompModule {
+object SocketModule {
 
     @Provides
     fun providesStompClient(
         okHttpWebSocketClient: OkHttpWebSocketClient
     ) = StompClient(okHttpWebSocketClient)
+
+    @Provides
+    @Singleton
+    fun providesGroupChatSocket(
+        stompClient: StompClient,
+        dataStorePreferences: DataStorePreferences
+    ) = GroupChatSocket(stompClient, dataStorePreferences)
 }
--- core/network/src/main/java/com/onmoim/core/network/socket/GroupChatSocket.kt
+++ core/network/src/main/java/com/onmoim/core/network/socket/GroupChatSocket.kt
@@ -1,13 +1,14 @@
 package com.onmoim.core.network.socket
 
+import android.util.Log
 import com.onmoim.core.datastore.DataStorePreferences
 import com.onmoim.core.datastore.PreferencesKey
 import com.onmoim.core.network.BuildConfig
 import com.onmoim.core.network.model.chat.ChatSendTextDto
 import com.onmoim.core.network.model.chat.SubscribeResponse
+import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.first
 import org.hildan.krossbow.stomp.StompClient
-import org.hildan.krossbow.stomp.StompSession
 import org.hildan.krossbow.stomp.conversions.kxserialization.StompSessionWithKxSerialization
 import org.hildan.krossbow.stomp.conversions.kxserialization.convertAndSend
 import org.hildan.krossbow.stomp.conversions.kxserialization.json.withJsonConversions
@@ -18,28 +19,39 @@ class GroupChatSocket @Inject constructor(
     private val stompClient: StompClient,
     private val dataStorePreferences: DataStorePreferences
 ) {
-    private lateinit var session: StompSession
-    private lateinit var jsonSession: StompSessionWithKxSerialization
+    companion object {
+        private val TAG = GroupChatSocket::class.java.simpleName
+    }
+
+    private lateinit var session: StompSessionWithKxSerialization
 
     suspend fun connect() {
         val accessToken = dataStorePreferences.getString(PreferencesKey.ACCESS_TOKEN).first()
             ?: throw Exception("accessToken is null")
         val headers = mapOf("Authorization" to accessToken)
         session = stompClient.connect(
-            url = BuildConfig.BASE_URL_WS,
+            url = "${BuildConfig.BASE_URL_WS}/ws-chat",
             customStompConnectHeaders = headers
-        )
-        jsonSession = session.withJsonConversions()
+        ).withJsonConversions()
+        subscribeSystemTopic()
     }
 
     suspend fun disconnect() {
         session.disconnect()
     }
 
-    suspend fun subscribe(destination: String) =
-        jsonSession.subscribe(destination, SubscribeResponse.serializer())
+    private suspend fun subscribeSystemTopic() {
+        subscribe("/system/queue").catch {
+            Log.e(TAG, "시스템 토픽 에러", it)
+        }.collect {
+            Log.d(TAG, "[SystemTopic] [${it.type}] ${it.content}")
+        }
+    }
 
-    suspend fun sendText(destination: String, chatSendTextDto: ChatSendTextDto) {
-        jsonSession.convertAndSend(destination, chatSendTextDto, ChatSendTextDto.serializer())
+    suspend fun subscribe(destination: String) =
+        session.subscribe(destination, SubscribeResponse.serializer())
+
+    suspend fun send(chatSendTextDto: ChatSendTextDto) {
+        session.convertAndSend("/app/chat.sendMessage", chatSendTextDto, ChatSendTextDto.serializer())
     }
 }
